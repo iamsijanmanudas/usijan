c7e83228ea6bbafc0a23d85108838537b7aa53c3
iamsijanmanudas <122923432+iamsijanmanudas@users.noreply.github.com>
2023-09-12 15:25:40 -0400

    image pahe, login and home working

--- src/main/java/comp31/usijan/MainController.java
+++ src/main/java/comp31/usijan/MainController.java
@@ -1,12 +1,17 @@
 package comp31.usijan;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+@Controller
 public class MainController {
-
+    @GetMapping("/")
+    public String getRoot() {
+        return "login";
+    }
+    
     @GetMapping("/login")
     public String getlogin(@RequestParam String username, @RequestParam String password, Model model) {
         model.addAttribute("username", username);
@@ -14,5 +19,10 @@ public class MainController {
 
         return "home";
     }
-    
+    @GetMapping("/image")
+public String getImage(Model model) {
+    model.addAttribute("name", "lol");
+    return "Imagepage";
+}
+
 }
