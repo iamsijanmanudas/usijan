adb82c3dc3ac4913556ee8d6618caae775376e04
iamsijanmanudas <122923432+iamsijanmanudas@users.noreply.github.com>
2023-09-13 16:41:16 -0400

    end of day

--- src/main/java/comp31/usijan/MainController.java
+++ src/main/java/comp31/usijan/MainController.java
@@ -20,13 +20,13 @@ public class MainController {
     public String redirectToDepartment(  @RequestParam String username, @RequestParam String password, Model model) {
         model.addAttribute("username", username);
         model.addAttribute("password", password);
-    Map<String, String> userDepartmentMap = new HashMap<>();
-    userDepartmentMap.put("Olivia", "Orders");
-    userDepartmentMap.put("Ollie", "Orders");
-    userDepartmentMap.put("Sam", "Sales");
-    userDepartmentMap.put("Sue", "Sales");
-    userDepartmentMap.put("Rachel", "Repairs");
-    userDepartmentMap.put("Ralph", "Repairs");
+        Map<String, String> userDepartmentMap = new HashMap<>();
+        userDepartmentMap.put("Olivia", "Orders");
+        userDepartmentMap.put("Ollie", "Orders");
+        userDepartmentMap.put("Sam", "Sales");
+        userDepartmentMap.put("Sue", "Sales");
+        userDepartmentMap.put("Rachel", "Repairs");
+        userDepartmentMap.put("Ralph", "Repairs");
 
     String departmentName = userDepartmentMap.getOrDefault(username, null);
     if (departmentName != null) {
