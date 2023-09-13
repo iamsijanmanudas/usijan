bc64f95180b338c4e2f778e816c2f41eeeb1997f
iamsijanmanudas <122923432+iamsijanmanudas@users.noreply.github.com>
2023-09-13 16:29:06 -0400

    end of departmental login

--- src/main/java/comp31/usijan/MainController.java
+++ src/main/java/comp31/usijan/MainController.java
@@ -1,5 +1,8 @@
 package comp31.usijan;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +14,29 @@ public class MainController {
     public String getRoot() {
         return "login";
     }
-    
+
+
     @GetMapping("/login")
-    public String getlogin(@RequestParam String username, @RequestParam String password, Model model) {
+    public String redirectToDepartment(  @RequestParam String username, @RequestParam String password, Model model) {
         model.addAttribute("username", username);
         model.addAttribute("password", password);
+    Map<String, String> userDepartmentMap = new HashMap<>();
+    userDepartmentMap.put("Olivia", "Orders");
+    userDepartmentMap.put("Ollie", "Orders");
+    userDepartmentMap.put("Sam", "Sales");
+    userDepartmentMap.put("Sue", "Sales");
+    userDepartmentMap.put("Rachel", "Repairs");
+    userDepartmentMap.put("Ralph", "Repairs");
 
+    String departmentName = userDepartmentMap.getOrDefault(username, null);
+    if (departmentName != null) {
+        model.addAttribute("departmentName", departmentName);
+        model.addAttribute("employeeName", username);
+        return departmentName.toLowerCase();
+    } else {
         return "home";
+    }
+
     }
     @GetMapping("/image")
 public String getImage(Model model) {
--- src/main/java/comp31/usijan/UsijanApplication.java
+++ src/main/java/comp31/usijan/UsijanApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UsijanApplication {
 
+	//https:localhost:8080/
 	public static void main(String[] args) {
 		SpringApplication.run(UsijanApplication.class, args);
 	}
